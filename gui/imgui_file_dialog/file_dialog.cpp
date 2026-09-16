5df07b0a8b4d0c7de11d8188ceff4b36712d14ca
Slashas632 <grnhz2091@pm.me>
2026-09-16 18:40:33 +0300

    fixed file dialog flickering bug

--- gui/imgui_file_dialog/file_dialog.cpp
+++ gui/imgui_file_dialog/file_dialog.cpp
@@ -7,10 +7,9 @@
 
 std::string filedialog_instance(settings_filedialog& settings) {
   std::string selected_path;
-  if (ImGuiFileDialog::Instance()->Display(settings.vKey, ImGuiWindowFlags_NoCollapse, settings.window_minSize, settings.window_maxSize)) {
+  if (ImGuiFileDialog::Instance()->Display(settings.vKey, ImGuiWindowFlags_NoCollapse, settings.window_minSize)) {
     if (ImGuiFileDialog::Instance()->IsOk()) {
       std::string selected_path = ImGuiFileDialog::Instance()->GetFilePathName();
-
       std::cout << "selected file: " << selected_path << "\n";
     }
     ImGuiFileDialog::Instance()->Close();
@@ -20,7 +19,7 @@ std::string filedialog_instance(settings_filedialog& settings) {
 
 void filedialog_button(settings_filedialog& settings) {
   if (ImGui::Button(settings.button_label.c_str())) {
-    settings.config.path = ".";
+      settings.config.path = ".";
     settings.config.countSelectionMax = 1;
     ImGuiFileDialog::Instance()->OpenDialog(settings.vKey, settings.vTitle,
                                             settings.filters.c_str(),
