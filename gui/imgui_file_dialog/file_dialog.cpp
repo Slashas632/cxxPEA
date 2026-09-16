ae622ff45ccb50d859a62e403f52fa6bb3767073
Slashas632 <grnhz2091@pm.me>
2026-09-16 22:12:44 +0300

    got rid of duplicate symbol error

--- gui/imgui_file_dialog/file_dialog.cpp
+++ gui/imgui_file_dialog/file_dialog.cpp
@@ -5,11 +5,17 @@
 
 #include "ImGuiFileDialog.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 std::string filedialog_instance(settings_filedialog& settings) {
   std::string selected_path;
-  if (ImGuiFileDialog::Instance()->Display(settings.vKey, ImGuiWindowFlags_NoCollapse, settings.window_minSize)) {
+  if (ImGuiFileDialog::Instance()->Display(settings.vKey,
+                                           ImGuiWindowFlags_NoCollapse,
+                                           settings.window_minSize)) {
     if (ImGuiFileDialog::Instance()->IsOk()) {
-      std::string selected_path = ImGuiFileDialog::Instance()->GetFilePathName();
+      std::string selected_path =
+          ImGuiFileDialog::Instance()->GetFilePathName();
       std::cout << "selected file: " << selected_path << "\n";
     }
     ImGuiFileDialog::Instance()->Close();
@@ -19,7 +25,7 @@ std::string filedialog_instance(settings_filedialog& settings) {
 
 void filedialog_button(settings_filedialog& settings) {
   if (ImGui::Button(settings.button_label.c_str())) {
-      settings.config.path = ".";
+    settings.config.path = ".";
     settings.config.countSelectionMax = 1;
     ImGuiFileDialog::Instance()->OpenDialog(settings.vKey, settings.vTitle,
                                             settings.filters.c_str(),
